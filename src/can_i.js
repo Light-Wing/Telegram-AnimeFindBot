644a121967115cbd130c40164de2301e49f98a4b
Light <light@lights-air.com>
2018-07-03 01:15:04 +0300

    d

--- src/can_i.js
+++ src/can_i.js
@@ -1,8 +1,11 @@
 'use strict';
 let _ = {}
-
+const wordsToInclude = [
+    'אפשר את האנימה',
+    'אפשר בבקשה את'
+]
 _ = (msg, type) => {
-    if (msg.text.includes("אפשר את")) {
+    if (wordsToInclude.includes(msg.text)) {
         msg.reply.text("לאחר שקראת את כלל 7 אשר כתוב בחוקי הקבוצה, חפש את הסדרה בגוגל או בקבוצה.", { asReply: true });
     }
 }
